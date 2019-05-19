47dfb1ecad860c0d11c35b28e580d4d4ffb70c76
apollo1986 <teyyubeyyuboglu@gmail.com>
2019-05-19 14:28:09 +0400

    addedd hyperlink test

--- POI_EXCEL/src/test/java/PoiExcelTest.java
+++ POI_EXCEL/src/test/java/PoiExcelTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Calendar;
 import java.util.Date;
+import org.apache.poi.common.usermodel.HyperlinkType;
 import org.apache.poi.hssf.usermodel.HSSFHeader;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
@@ -361,13 +362,68 @@ public class PoiExcelTest {
     public void testColumnWidth() {
 
 //        sheet = wb.createSheet();
-
         Row row = sheet.createRow(3);
         Cell cell = row.createCell(1);
         cell.setCellValue("F4asdasd sdasddasdasdasd");
         // Auto-size the columns.
         sheet.autoSizeColumn(0);
         sheet.autoSizeColumn(1);
+    }
+
+    public void createHiperlink() {
+//        Workbook wb = new XSSFWorkbook(); //or new HSSFWorkbook();
+        CreationHelper createHelper = wb.getCreationHelper();
+
+        //cell style for hyperlinks
+        //by default hyperlinks are blue and underlined
+        CellStyle hlink_style = wb.createCellStyle();
+        Font hlink_font = wb.createFont();
+        hlink_font.setUnderline(Font.U_SINGLE);
+        hlink_font.setColor(IndexedColors.BLUE.getIndex());
+        hlink_style.setFont(hlink_font);
+
+        Cell cell;
+//        Sheet 
+        sheet = wb.createSheet("Hyperlinks");
+        //URL
+        cell = sheet.createRow(0).createCell(0);
+        cell.setCellValue("URL Link");
+
+        Hyperlink link = createHelper.createHyperlink(HyperlinkType.URL);
+        link.setAddress("http://poi.apache.org/");
+        cell.setHyperlink(link);
+        cell.setCellStyle(hlink_style);
+
+        //link to a file in the current directory
+        cell = sheet.createRow(1).createCell(0);
+        cell.setCellValue("File Link");
+        link = createHelper.createHyperlink(HyperlinkType.FILE);
+        link.setAddress("link1.xls");
+        cell.setHyperlink(link);
+        cell.setCellStyle(hlink_style);
+
+        //e-mail link
+        cell = sheet.createRow(2).createCell(0);
+        cell.setCellValue("Email Link");
+        link = createHelper.createHyperlink(HyperlinkType.EMAIL);
+        //note, if subject contains white spaces, make sure they are url-encoded
+        link.setAddress("mailto:poi@apache.org?subject=Hyperlinks");
+        cell.setHyperlink(link);
+        cell.setCellStyle(hlink_style);
+
+        //link to a place in this workbook
+        //create a target sheet and cell
+//        Sheet
+        sheet2 = wb.createSheet("Target Sheet");
+        sheet2.createRow(0).createCell(0).setCellValue("Target Cell");
+
+        cell = sheet.createRow(3).createCell(0);
+        cell.setCellValue("Worksheet Link");
+        Hyperlink link2 = createHelper.createHyperlink(HyperlinkType.DOCUMENT);
+        link2.setAddress("'Target Sheet'!A1");
+        cell.setHyperlink(link2);
+        cell.setCellStyle(hlink_style);
+
     }
 
     @Test
@@ -384,7 +440,8 @@ public class PoiExcelTest {
 //        repeatingRowAndColumn();
 //        headerAndFooter();
 //        commentCell();
-        testColumnWidth();
+//        testColumnWidth();
+        createHiperlink();
         try (OutputStream fileOut = new FileOutputStream(file_name)) {
             wb.write(fileOut);
         }
